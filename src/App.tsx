1b26bf786ea2140a7323b2ed1e275ab6dcbf8469
zhoupan <zhoupan@zeaho.com>
2022-07-04 16:34:58 +0800

    fix(pages): fix bugs

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-console.log('111')
+console.log('111222')
   return (
     <div className="App">
       <header className="App-header">

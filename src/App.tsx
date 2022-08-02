9dc7d36b0c02d1e5ec34d54f817571b9698a95e7
zhoupan <zhoupan@zeaho.com>
2022-08-02 11:32:05 +0800

    完成button组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Button, { ButtonType, ButtonSize } from './components/Button/button'
+import './styles/index.scss';
 
-function App() {
-console.log('111222')
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <Button disabled>Hello</Button>
+        <Button disabled>Disabled Button</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+          Large Primary
+        </Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>
+          Small Danger
+        </Button>
+        <Button btnType={ButtonType.Link} href='http://www.baidu.com'>
+          Baidu Link
+        </Button>
+        <Button btnType={ButtonType.Link} href='http://www.baidu.com' disabled>
+          Disabled Link
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
